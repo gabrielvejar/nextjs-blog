69897fb377b9ac68244bf57291e417efb6d05f1f
Gabriel Vejar <g.vejarperez@gmail.com>
2023-01-21 19:07:06 -0300

    first commit

---
+++ components/date.js
@@ -0,0 +1,9 @@
+import { parseISO, format, Locale } from 'date-fns';
+// import { es } from 'date-fns/locale';
+
+export default function Date({ dateString }) {
+  const date = parseISO(dateString);
+  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy'
+//   , {locale: es}
+  )}</time>;
+}
